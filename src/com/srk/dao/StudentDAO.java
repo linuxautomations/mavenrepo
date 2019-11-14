8a2795f42daa85213c8e58136a9ad4bbbc774f4d
KalluruS <>
2019-11-14 15:58:02 +0530

    --- Auto-Git Commit ---

--- src/com/srk/dao/StudentDAO.java
+++ src/com/srk/dao/StudentDAO.java
@@ -37,7 +37,8 @@ public class StudentDAO {
         try{  
             Connection con=StudentDAO.getConnection();  
             PreparedStatement ps=con.prepareStatement("insert into Students(student_name,student_gender,student_addr,student_age,student_qual,student_percent,student_year_passed) values (?,?,?,?,?,?)");  
-            ps.setString(1,std.getStudentName());  
+            ps.setString(1,std.getStudentName());
+              
             ps.setString(2,std.getStudentAddr());  
             ps.setString(3,std.getAge());  
             ps.setString(4,std.getQualification());  
